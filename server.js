8f72df4593bbd91f7b8ee1764fa1e046b4670aba
Raghul PK <pkraghul2001@gmail.com>
2023-10-21 18:24:40 +0530

    Changing port variable for deployment

--- server.js
+++ server.js
@@ -145,6 +145,7 @@ io.on('connection', (socket) => {
 
 });
 
-server.listen(3000, () => {
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
   console.log('server running at http://localhost:3000');
 });
